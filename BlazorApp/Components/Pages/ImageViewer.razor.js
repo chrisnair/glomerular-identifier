8ba31758b5d18f42398f06d6b085fffc3172258c
Christopher Nair <chrisnair1@gmail.com>
2024-09-27 22:40:57 -0500

    in progress jsinterop

---
+++ BlazorApp/Components/Pages/ImageViewer.razor.js
@@ -0,0 +1,10 @@
+// function initializeOpenSeadragon(containerId, options) {
+//     console.log("called")
+//     OpenSeadragon(Object.assign({ id: containerId }, options));
+// }
+
+function log(message) {
+    return console.log(message);
+}
+
+export { log};

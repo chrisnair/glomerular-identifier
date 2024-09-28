ef64a6e0e2d9c654b2b376b9674dac8b839084b3
Christopher Nair <chrisnair1@gmail.com>
2024-09-27 23:19:49 -0500

    Removed debug relics

--- BlazorApp/Components/Pages/ImageViewer.razor.js
+++ BlazorApp/Components/Pages/ImageViewer.razor.js
@@ -1,10 +1,5 @@
 export function initializeOpenSeadragon(containerId, options) {
-    console.log("called")
     OpenSeadragon(Object.assign({ id: containerId }, options));
-}
-
-export function log(message) {
-    return console.log(message);
 }
 
 
